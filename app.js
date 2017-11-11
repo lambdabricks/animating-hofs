6d12aa8226e84d5a378cfa46feba4c1fab81993a
Daniel Garcia <pel_daniel@hotmail.com>
2017-11-11 18:35:41 -0200

    Extract 'collection_enters' animation in its own function

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
-var animateButton = document.getElementById('animate');
+let animateButton = document.getElementById('animate');
 
-animateButton.onclick = function(event) {
-  var timeline = new TimelineLite();
+animateButton.addEventListener('click', function(event) {
+  let master = new TimelineLite();
 
-  timeline.to(
-    "#input-collection",
-    1.2,
-    {
-      y: 59,
-      scale: 0.7
-    }
-  );
-  timeline.to(
-    "#input-collection",
-    1.2,
-    {
-      y: 100,
-      scale: 1
-    }
-  );
+  master
+    .add(collection_enters());
+
+});
+
+const collection_enters = function() {
+  let timeline = new TimelineLite();
+
+  timeline
+    .to('#input-collection', 1.2, { y: 59, scale: 0.6 }, 'collection_enters')
+    .to('#input-collection', 1.2, { y: 100, scale: 1 })
+    .to('#input-slot1', 0.4, { scale: 1.3 }, 'collection_enters+=0.25')
+    .to('#input-slot1', 0.4, { scale: 1 }, 'collection_enters+=1.5');
+
+  return timeline;
 };
