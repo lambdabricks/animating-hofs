5d2077f02b3744576cf1cd90d064dd419c945c02
Daniel Garcia <pel_daniel@hotmail.com>
2017-11-21 19:38:27 -0200

    Refactor animation Timelines for adding new hof's

--- app.js
+++ app.js
@@ -9,59 +9,65 @@ let data = {
   selected_function: 'map'
 };
 
-let animateButton = document.getElementById('animate');
+// html elements
+const input_collection = document.getElementById('input-collection');
+const collection_elements = input_collection.getElementsByClassName('input-collection-element');
+const collection_element = collection_elements[0];
+const input_function = document.getElementById('input-function');
+const input_function_slot = input_function.getElementsByClassName('brick-slot')[0];
+
+const main_brick = document.getElementById('mainbrick');
+const slot_collection = document.getElementById('mb-slot-collection');
+const slot_function = document.getElementById('mb-slot-function');
+const output_slot = document.getElementById('mb-output-slot');
+
+
+const animateButton = document.getElementById('animate');
 
 animateButton.addEventListener('click', function(event) {
   let master = new TimelineLite();
 
   if(data.selected_function == 'map') {
     master
-      .add(collection_enters())
+      .add(position_collection(collection_position_for_map()))
       .add(position_input_function())
       .add(map_collection())
       .add(output_collection_result());
   } else if(data.selected_function == 'reduce') {
-
+    master
+      .add(position_collection(position_for_reduce_hof()))
+      .add(position_input_function())
+      .add(map_collection())
+      .add(output_collection_result());
   }
 });
 
-const collection_enters = function() {
+const position_collection = function(final_x_position) {
   const timeline = new TimelineLite();
-  const main_brick = document.getElementById('mainbrick');
-  const input_collection = document.getElementById('input-collection');
-  const slot1 = document.getElementById('mb-slot-collection');
-
-  const left_offset = input_collection.offsetLeft - main_brick.offsetLeft;
-  const centered = main_brick_half_width(input_collection, main_brick) - left_offset;
 
   timeline
     .to(input_collection, 1.2, { y: 105, scale: 0.6, ease: Sine.easeIn }, 'collection_enters')
-    .to(input_collection, 0.8, { y: 155, scale: 1 })
-    .to(slot1, 0.4, { scale: 1.3 }, 'collection_enters+=0.25')
-    .to(slot1, 0.4, { scale: 1 }, 'collection_enters+=1.5')
-    .to(input_collection, 0.8, { x: centered });
+    .to(input_collection, 0.8, { y: 140, scale: 1 })
+    .to(slot_collection, 0.4, { scale: 1.3 }, 'collection_enters+=0.25')
+    .to(slot_collection, 0.4, { scale: 1 }, 'collection_enters+=1.5')
+    .to(input_collection, 0.8, { x: final_x_position })
+    .to(input_collection, 0.4, { y: 155 });
 
   return timeline;
 };
 
 const position_input_function = function() {
   const timeline = new TimelineLite();
-  const input_function = document.getElementById('input-function');
-  const input_slot = input_function.getElementsByClassName('brick-slot')[0];
-  const slot2 = document.getElementById('mb-slot-function');
 
-  const main_brick = document.getElementById('mainbrick');
-  const input_collection = document.getElementById('input-collection');
-  const collection_element = input_collection.getElementsByClassName('input-collection-element')[0];
-  const left_offset = input_function.offsetLeft + input_slot.offsetLeft -
-    ((collection_element.offsetWidth - input_slot.offsetWidth) / 2) -
+  const left_offset = input_function.offsetLeft + input_function_slot.offsetLeft -
+    ((collection_element.offsetWidth - input_function_slot.offsetWidth) / 2) -
     (main_brick_half_width(input_collection, main_brick) + main_brick.offsetLeft);
 
   timeline
     .to(input_function, 1.2, { y: 105, scale: 0.6, ease: Sine.easeIn }, 'function_enters')
     .to(input_function, 1.2, { y: 190, scale: 1 })
-    .to(slot2, 0.4, { scale: 1.3 }, 'function_enters+=0.25')
-    .to(slot2, 0.4, { scale: 1 }, 'function_enters+=1.5')
+    .to(slot_function, 0.4, { scale: 1.3 }, 'function_enters+=0.25')
+    .to(slot_function, 0.4, { scale: 1 }, 'function_enters+=1.5')
     .to(input_function, 1.2, { x: '-=' + left_offset });
 
   return timeline;
@@ -69,9 +75,6 @@ const position_input_function = function() {
 
 const map_collection = function() {
   const timeline = new TimelineLite();
-  const input_collection = document.getElementById('input-collection');
-  const collection_elements = input_collection.getElementsByClassName('input-collection-element');
-  const input_brick = document.getElementById('input-function');
   const total_elements = collection_elements.length;
 
   for(i = 0; i < total_elements; i++) {
@@ -88,7 +91,7 @@ const map_collection = function() {
 
     // No need to move the input brick after the last element
     if(i != total_elements - 1) {
-      timeline.to(input_brick, 0.4, { x: '+=' + element.offsetWidth });
+      timeline.to(input_function, 0.4, { x: '+=' + element.offsetWidth });
     }
   }
 
@@ -97,8 +100,6 @@ const map_collection = function() {
 
 const output_collection_result = function() {
   const timeline = new TimelineLite();
-  const input_collection = document.getElementById('input-collection');
-  const output_slot = document.getElementById('output-slot');
 
   timeline
     .to(input_collection, 1.2, { y: '+=100', scale: 0.6, ease: Sine.easeIn }, 'collection_exits')
@@ -108,6 +109,13 @@ const output_collection_result = function() {
 
   return timeline;
 }
+
+const collection_position_for_map = function() {
+  const left_offset = input_collection.offsetLeft - main_brick.offsetLeft;
+  const centered = main_brick_half_width(input_collection, main_brick) - left_offset;
+
+  return centered;
+};
 
 const main_brick_half_width = function(input_collection, main_brick) {
   return ((main_brick.offsetWidth - input_collection.offsetWidth) / 2);
